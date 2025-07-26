24de15a40e54d2b3dd40a9aaef4b2ce025776a0c
yuna <yuna5470@naver.com>
2025-07-26 17:23:15 +0900

    회원가입 추가

--- src/Doodle.jsx
+++ src/Doodle.jsx
@@ -8,6 +8,13 @@ export default function Doodle() {
     const closebtn = document.querySelector(".closebtn")
     const loginbtn = document.getElementById("loginbtn")
 
+    const signupLink = document.getElementById("signupLink")
+    const signupModal = document.getElementById("signmodal")
+    const closebtn2 = document.querySelector(".closebtn2")
+    const signupbtn = document.getElementById("signupbtn")
+
+    
+
 const handleLogin = () =>{
     const username = document.getElementById("username").value
     const password = document.getElementById("password").value
@@ -31,6 +38,33 @@ const handleLogin = () =>{
             console.error("에러 발생: ",err)
         })
     }
+    const handleSignup = () => {
+        const username = document.getElementById("signupusername").value
+        const password = document.getElementById("signuppassword").value
+
+        fetch("http//localHost:8080/signup",{
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body : JSON.stringify({username, password})
+        })
+            .then((res)=>res.json())
+            .then((data)=>{
+                if (data.success){
+                    alert("회원가입 완료 되었습니다. 로그인 후 이용해주세요.")
+                    signupModal.style.display = "none"
+                }else{
+                    alert("회원가입 실패: "+data.message)
+                }
+            })
+                .catch((err)=>{
+                    console.error("에러 발생: ",err)
+                })
+                if (loginbtn){
+        loginbtn.addEventListener("click", handleLogin)
+    }
+    }
     if (loginLink && loginModal && closebtn){
         loginLink.addEventListener("click", (e)=>{
             e.preventDefault()
@@ -45,8 +79,20 @@ const handleLogin = () =>{
             }
         })
     }
-    if (loginbtn){
-        loginbtn.addEventListener("click", handleLogin)
+    if(signupLink && signupModal && closebtn2){
+        signupLink.addEventListener("click",(e)=>{
+            e.preventDefault()
+            signupModal.style.display="block"
+        })
+        closebtn2.addEventListener("click",()=>{
+            signupModal.style.display = "none"
+        })
+        window.addEventListener("click",(e)=>{
+            if (e.target === signupModal){
+                signupModal.style.display = "none"
+            }
+        })
+    if (signupbtn) signupbtn.addEventListener("click",handleSignup)
     }
  },[])
   return (
@@ -64,7 +110,7 @@ const handleLogin = () =>{
         <div className="nav2">
              <ul className="ul2">
              <li className="li2"><a href="#" className="a" id="loginLink">로그인</a></li>
-            <li className="li2"><a href="#" className="a">회원가입</a></li>
+            <li className="li2"><a href="#" className="a" id="signupLink">회원가입</a></li>
           </ul>
         </div>
         <div className="icon">
@@ -78,6 +124,15 @@ const handleLogin = () =>{
                 <input type="text" id="username" className="logininput" placeholder="아이디"/>
                 <input type="password" id="password" className="logininput" placeholder="비밀번호"/>
                 <button id="loginbtn" className="loginbtn">로그인</button>
+            </div>
+        </div>
+        <div id="signmodal" className="modal">
+            <div className="modal-content">
+                <span className="closebtn2">&times;</span>
+                <h2 className="madaltitle">회원가입</h2>
+                <input type="text" id="signupusername" className="logininput" placeholder="아이디"/>
+                <input type="password" id="signuppassword" className="logininput" placeholder="비밀번호"/>
+                <button id="signupbtn" className="loginbtn">회원가입</button>
             </div>
         </div>
     </div>
