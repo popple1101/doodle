e24d3cb690584ef51968b495066f5d80a2dd6c17
yuna <yuna5470@naver.com>
2025-07-26 16:42:55 +0900

    포트 수정

--- src/Doodle.jsx
+++ src/Doodle.jsx
@@ -12,7 +12,7 @@ const handleLogin = () =>{
     const username = document.getElementById("username").value
     const password = document.getElementById("password").value
 
-    fetch("http://localhost:3000/login",{
+    fetch("http://localhost:8080/login",{
         method: "POST", headers: {
             "Content-Type":"application/json"
         },
